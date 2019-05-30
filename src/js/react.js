faa20e3a739ffebdd7e7a19b1d1bad9a16b4a629
Ádám András <adm.andras@gmail.com>
2019-05-30 20:32:53 +0200

    Removed packages

--- src/js/react.js
+++ src/js/react.js
@@ -1,18 +1,12 @@
 //
-// React Test Bench - Frontend (React), 2019
+// Date Picker - Frontend (React), 2019
 //
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from '../components/app_reducer';
-
 import '../scss/style.scss';
 
 import App from '../components/App';
 
-const store = createStore(reducer);
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
