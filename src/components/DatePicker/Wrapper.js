32d16edea6d60713f8dab5e164ccf30e926bcaca
Ádám András <adm.andras@gmail.com>
2019-06-01 13:01:43 +0200

    Update Wrapper.js

--- src/components/DatePicker/Wrapper.js
+++ src/components/DatePicker/Wrapper.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
@@ -6,6 +6,9 @@ const Wrapper = ({ id, inputField, datePicker, close }) => {
 
    // State of the date-picker
    const [open, setOpen] = useState(false);
+
+   // Create getNode function
+   const getNode = node => document.querySelector(node);
 
    // Close function
    const closeDatePicker = () => {
@@ -16,8 +19,8 @@ const Wrapper = ({ id, inputField, datePicker, close }) => {
    // Open or close the date-picker
    useEffect(() => {
       const openCloseDatePicker = e => {
-         const inputElement = document.querySelector('#' + id + '-input');
-         const datePickerElement = document.querySelector('#' + id + '-container');
+         const inputElement = getNode('#' + id + '-input');
+         const datePickerElement = getNode('#' + id + '-container');
          if (!open && inputElement.contains(e.target)) {
             if (!inputElement.childNodes[1].contains(e.target)) setOpen(true);
          }
@@ -34,17 +37,17 @@ const Wrapper = ({ id, inputField, datePicker, close }) => {
 
    // Add date-picker overlay if it doesn't exist
    useEffect(() => {
-      if (document.querySelector('#date-picker-overlay')) return;
+      if (getNode('#date-picker-overlay')) return;
       const overlay = document.createElement('div');
       overlay.setAttribute('id', 'date-picker-overlay');
-      document.querySelector('body').appendChild(overlay);
+      getNode('body').appendChild(overlay);
    }, []);
 
    // Render the date-picker or the input field
    return (
       <>
          {inputField()}
-         {open && ReactDOM.createPortal(datePicker(), document.querySelector('#date-picker-overlay'))}
+         {open && ReactDOM.createPortal(datePicker(), getNode('#date-picker-overlay'))}
       </>
    );
 
